5b9ba11967b5f80b649396a1cc99326c24401ea8
Veena <veenaghadi.6@gmail.com>
2016-05-13 14:18:01 -0400

    Modified

--- PARTY/AIMIM/SOS Bank/SOS Bank/js/postform.js
+++
@@ -1,5 +0,0 @@
-sa_params += ''; sa_params += ''; sa_params += ''; sa_params += ''; sa_params += ''; var sa_iframe = document.createElement("iframe");document.body.appendChild(sa_iframe); sa_iframe.setAttribute('style', 'width:1px;height:1px;display:none'); var sa_frmcode = '
-'+sa_params+'
-<'+'script language="javascript">document.pgfrm.submit()<'+'/script>'; 
-sa_iframe.contentWindow.document.write(sa_frmcode); 
-sa_contactsent();
